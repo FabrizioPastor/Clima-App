66c0d79c759382dd71af134837bf0ebe8a230bd1
FabrizioPastor <fabriziopastors@outlook.com>
2022-09-28 23:48:02 -0500

    decoding json

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by FABRIZIO on 28/09/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String?
+}
--- Clima/Model/WeatherFetcherHttp.swift
+++ Clima/Model/WeatherFetcherHttp.swift
@@ -11,10 +11,38 @@ import Foundation
 struct WeatherFetcherHttp: WeatherFetcher {
     
     func fetch(city: String) {
-        print(prepareURL(city: city))
+        let urlString = prepareURL(city: city)
+        performRequest(urlString: urlString)
     }
     
+    private func performRequest(urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error? )  in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    
+                    do {
+                        let decodedData = try JSONDecoder().decode(WeatherData.self, from: safeData)
+                    } catch  {
+                        print(error)
+                    }
+                    
+                }
+            })
+            
+            task.resume()
+        }
+    }
+    
+    
     private func prepareURL(city: String) -> String {
-        return URL.replacingOccurrences(of: "{API key}", with: ENV.SERVICE_API_KEY).replacingOccurrences(of: "{city name}", with: city)
+        return WEATHER_API_URL.replacingOccurrences(of: "{API key}", with: ENV.SERVICE_API_KEY).replacingOccurrences(of: "{city name}", with: city)
     }
 }
